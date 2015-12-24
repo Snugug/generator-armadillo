fc4cc9c100910ace4fe79da30942f2e68c7cb644
Sam Richard <snugug@gmail.com>
2015-12-24 14:07:04 -0500

    Add Armadillo helper

---
+++ helpers/armadillo.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var chalk = require('chalk');
+var counter = 0;
+
+module.exports = function (text) {
+  var border = Array(text.length + 3).join('-');
+  var armadillo;
+
+  //////////////////////////////
+  // Armadillo ASCII Art
+  //////////////////////////////
+  if (counter === 0) {
+    armadillo =
+      chalk.red('\n               ,.-----__') +
+      chalk.red('\n            ,:::://///,:::-.') +
+      chalk.red('\n          /:\'\'/////// ``:::`;/|/') + chalk.magenta('     .' + border + '.') +
+      chalk.red('\n         /\'   ||||||     :://\'`\\') + chalk.magenta('     | ') + chalk.yellow.bold(text) + chalk.magenta(' |') +
+      chalk.red('\n        .\' ,   ||||||     `/(  ') + chalk.white('e') + chalk.red(' \\') + chalk.magenta('   /' + border + '\'') +
+      chalk.red('\n  -===~__-\'\\__X_`````\\_____/~`-._ `.') + chalk.magenta('  ') +
+      chalk.red('\n              ~~        ~~       `~-\'') + '\n';
+  } else {
+    armadillo =
+      chalk.red('\n:-.') +
+      chalk.red('\n::`;/|/') + chalk.magenta('     .' + border + '.') +
+      chalk.red('\n:://\'`\\') + chalk.magenta('     | ') + chalk.yellow.bold(text) + chalk.magenta(' |') +
+      chalk.red('\n `/(  ') + chalk.white('e') + chalk.red(' \\') + chalk.magenta('   /' + border + '\'') +
+      chalk.red('\n__/~`-._ `.') + chalk.magenta('  ') +
+      chalk.red('\n~       `~-\'') + '\n';
+  }
+
+  counter++;
+  return armadillo;
+};
